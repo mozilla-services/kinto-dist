eb8791ad28ba6ae5e24dff98fed72f8cec088de4
Mathieu Leplatre <mathieu@mozilla.com>
2021-12-03 09:59:33 +0100

    Restore version fields signer and changes capabilities metadata

--- kinto_remote_settings/__init__.py
+++ kinto_remote_settings/__init__.py
@@ -1,3 +1,10 @@
+import pkg_resources
+
+# Note: we can remove this and replace it with a better package name
+# when doing https://github.com/mozilla-services/kinto-dist/issues/1928
+__version__ = pkg_resources.get_distribution("kinto-dist").version
+
+
 def includeme(config):
     config.include("kinto_remote_settings.changes")
     config.include("kinto_remote_settings.signer")
